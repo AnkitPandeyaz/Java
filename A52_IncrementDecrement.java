cb56fc0cd85186e05e87e11db36682d08a04aae2
Ankit Pandey <pandeyankit713@gmail.com>
2023-02-23 01:30:59 +0530

    new Java file

---
+++ A52_IncrementDecrement.java
@@ -0,0 +1,25 @@
+//This program demonstrates the (++) Increment and (--) Decrement operators (also called postfix).
+
+public class A52_IncrementDecrement {
+    public static void main(String [] args){
+
+        int number =5;
+
+        //Display the value in number
+        System.out.println("Number is " + number);
+        System.out.println("I will Increment number.");
+
+        //increment number
+
+        number++;
+
+        //Display the value in number again
+
+        System.out.println("Now, Number is " + number);
+        System.out.println("I will decrement number.");
+
+        number--;
+
+    }
+    
+}
